c7a054b8539927f4275f1f9c2008a6d5230e657e
Jordan <139855911+krishnayah@users.noreply.github.com>
2024-07-02 18:56:46 -0400

    Removed CoreML, using metal native.

--- undertone.py
+++ undertone.py
@@ -1,13 +1,5 @@
 import os
 
-def install_dependencies():
-    # installs all dependencies
-    # run command pip install
-    os.system("python3 -m pip install ane_transformers")
-    os.system("python3 -m pip install openai-whisper")
-    os.system("python3 -m pip install coremltools")
-
-    pass
 def whispercpp_installed():
     # determines if whispercpp is available.
     return os.path.exists("whisper.cpp")
@@ -17,22 +9,7 @@ def download_whispercpp():
     # https://github.com/ggerganov/whisper.cpp
     # downloads whispercpp
     os.system("git clone https://github.com/ggerganov/whisper.cpp.git")
-
-
-
-def coreml_model_available(size):
-    # determines if a given model is available
-    return os.path.exists(f'./whisper.cpp/models/ggml-{size}.en-encoder.mlmodelc')
-
 def is_apple_silicon():
-    pass
-
-def generate_coreml_model(size):
-    os.system(f"./whisper.cpp/models/generate-coreml-model.sh {size}.en")
-    pass
-
-def build_whispercpp_coreml():
-    os.system("cd whisper.cpp && make clean && WHISPER_COREML=1 make -j")
     pass
 
 def run_model(audio):
@@ -42,4 +19,17 @@ def download_video(url):
     # downloads a video, converts to Wav 16k and returns the filepath
     pass
 
-download_whispercpp()
+def download_model(size):
+    os.system(f"cd whisper.cpp && make {size}")
+
+def make_model(size):
+    pass
+
+
+
+if not whispercpp_installed():
+    print("Whispercpp not downloaded. Downloading now.")
+    download_whispercpp()
+
+download_model("small")
+os.system("cd whisper.cpp && make -j && ./main -m ./models/ggml-small.bin -f ./samples/jfk.wav -t 16")
