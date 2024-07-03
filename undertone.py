4ffa03c9e0ca616bdd5a6901037cb22c6e65ada5
Jordan <139855911+krishnayah@users.noreply.github.com>
2024-07-03 00:12:04 -0400

    Works! Transcribes youtube videos properly and saves them to a .txt file.

--- undertone.py
+++ undertone.py
@@ -1,4 +1,20 @@
 import os
+import yt_dlp
+
+
+ytdlp_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+    'postprocessor_args': [
+        '-ar', '16000'
+    ],
+    'outtmpl': 'audiofiles/%(title)s.%(ext)s',
+    'restrictfilenames': True,
+}
 
 def whispercpp_installed():
     # determines if whispercpp is available.
@@ -9,22 +25,25 @@ def download_whispercpp():
     # https://github.com/ggerganov/whisper.cpp
     # downloads whispercpp
     os.system("git clone https://github.com/ggerganov/whisper.cpp.git")
-def is_apple_silicon():
-    pass
+    os.system("cd whisper.cpp && make -j")
 
-def run_model(audio):
+
+def run_model(audio, size):
+    os.system(f"cd whisper.cpp && ./main -m ./models/ggml-{size}.bin -f '../{audio}' -t 16 -otxt")
+
     pass
 
 def download_video(url):
     # downloads a video, converts to Wav 16k and returns the filepath
+    with yt_dlp.YoutubeDL(ytdlp_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        audio_filename = ydl.prepare_filename(info).replace("webm", "wav")
+
+        return audio_filename
     pass
 
 def download_model(size):
     os.system(f"cd whisper.cpp && make {size}")
-
-def make_model(size):
-    pass
-
 
 
 if not whispercpp_installed():
@@ -32,4 +51,5 @@ if not whispercpp_installed():
     download_whispercpp()
 
 download_model("small")
-os.system("cd whisper.cpp && make -j && ./main -m ./models/ggml-small.bin -f ./samples/jfk.wav -t 16")
+filename = download_video("https://www.youtube.com/watch?v=FgX5mGCnt64")
+run_model(filename, "small")
